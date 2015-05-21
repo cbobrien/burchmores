c81cf991d07c29bc4128adda75232933fcdbde83
Barratt <b4rr4tt@gmail.com>
2015-05-21 14:43:52 +0200

    Form checkbox, css changes

--- resources/views/pages/finance-enquiry.blade.php
+++ resources/views/pages/finance-enquiry.blade.php
@@ -1,54 +1,102 @@
-@include ('errors.list')
+@extends('layouts.master')
 
-	{!! Form::open(['route' => 'finance-enquiry.send', 'class' => 'form-horizontal']) !!}
-		<div class="form-group">
-			<label for="city_id">Region</label>
-			<div class="col-md-6">
-				{!! Form::select('city_id', [null => 'Please Select'] + $cities, null, ['class' => 'form-control']) !!}
-			</div>
-		</div>
+@section('content')
 
-		<div class="form-group">
-			<label for="first_name">First Name</label>			
-			{!! Form::text('first_name', null, ['class' => 'form-control']) !!}		
-		</div>   
+<div class="relative white-container">
 
-		<div class="form-group">
-			<label for="surname">Surname</label>			
-			{!! Form::text('surname', null, ['class' => 'form-control']) !!}		
-		</div>    
+	<h1 class="page-header">Finance Enquiry Form</h1>
 
-		<div class="form-group">
-			<label for="surname">Contact Number</label>			
-			{!! Form::text('telephone', null, ['class' => 'form-control']) !!}		
-		</div>      
+	@include ('errors.list')
 
-		<div class="form-group">
-			<label for="surname">Work Number</label>			
-			{!! Form::text('work_telephone', null, ['class' => 'form-control']) !!}		
-		</div>       
- 
- 		<div class="form-group">
-			<label for="surname">Email Address</label>			
-			{!! Form::text('email', null, ['class' => 'form-control']) !!}		
-		</div>  
-		
-		<div class="form-group">
-			<p>Which Do You Require?</p>
-			{!! Form::radio('requirements', 'Finance') !!}	Finance <br>
-			{!! Form::radio('requirements', 'Warranties') !!} Warranties <br>
-			{!! Form::radio('requirements', 'Both') !!}	 Both <br>
-		</div>
+	<div class="form-container">
 
-		<div class="form-group">
-			<label for="surname">Comments</label>			
-			{!! Form::textarea('comments', null, ['class' => 'form-control']) !!}		
-		</div>  
+		{!! Form::open(['route' => 'finance-enquiry.send', 'class' => 'form-horizontal']) !!}
 
-		<div class="form-group">
-			<div class="">
-				<button type="submit" class="btn btn-primary">Send</button>
-			</div>
-		</div>
+			<div class="row">
+				<div class="col-md-12">
+					<h3>Region</h3>
+					{!! Form::select('city_id', [null => 'Please Select'] + $cities, null) !!}
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row -->		
 
-	{!! Form::close() !!}
+			<div class="row">
+				<div class="col-md-12">
+					<h3>First Name</h3>
+					{!! Form::text('first_name', null) !!}	
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row -->	
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3>Surname</h3>
+					{!! Form::text('surname', null) !!}
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row -->		
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3>Contact Number</h3>
+					{!! Form::text('telephone', null) !!}
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row -->	
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3>Work Number</h3>
+					{!! Form::text('work_telephone', null) !!}
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row -->	
+
+			<div class="row">
+				<div class="col-md-12">
+					<h3>Email Address</h3>
+					{!! Form::email('email', null) !!}
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row -->														          
+			
+			<div class="row">
+				<div class="col-md-12">
+				<h3>Which Do You Require?</h3>
+				{!! Form::radio('requirements', 'Finance') !!}	Finance <br>
+				{!! Form::radio('requirements', 'Warranties') !!} Warranties <br>
+				{!! Form::radio('requirements', 'Both') !!}	 Both <br>
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row -->
+
+			<div class="row">
+				<div class="col-md-12">
+				<h3>Comments</h3>		
+				{!! Form::textarea('comments', null) !!}
+				</div><!-- //.col-md-12 -->
+			</div><!-- //.row --> 
+
+			<div class="row padded">
+				<div class="col-md-8">
+					<div class="row">
+						<div class="col-md-12">
+							* Indicates a required field, and needs to be completed.	
+						</div><!-- //.col-md-12 -->
+					</div>
+					<div class="row">
+						<div class="col-md-12 terms-container">
+							<div class="checkbox-container">
+								<input type="checkbox" class="terms-checkbox" name="terms" />
+							</div><!-- //.chechbox-container -->
+							
+								I have read and understand the terms and conditions and privacy statement of this website.
+							
+						</div><!-- //.col-md-12 -->
+					</div><!-- //.row -->
+				</div><!-- //.col-md-8 -->
+				<div class="col-md-4">
+					<input type="submit" class="submit green-btn" value="SUBMIT" />
+				</div><!-- //.col-md-4 -->
+			</div><!-- //.row -->			
+
+		{!! Form::close() !!}
+
+		</div><!-- //.form-container -->	
+
+	</div><!-- //.relative -->
+
+@stop
